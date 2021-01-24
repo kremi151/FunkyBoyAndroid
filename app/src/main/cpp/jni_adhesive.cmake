767d9baf808efef1efe50a6a46bcea2efb4fd0b6
Michel Kremer <kremi151dev@gmail.com>
2021-01-24 17:34:03 +0100

    Remove debug logs

--- app/src/main/cpp/jni_adhesive.cmake
+++ app/src/main/cpp/jni_adhesive.cmake
@@ -3,14 +3,10 @@ set(FB_ANDROID_DYNAMIC_INCLUDE_DIR "${CMAKE_BINARY_DIR}/generated/include")
 macro(fb_generate_strings_cpp)
     file(STRINGS ../res/values/strings.xml FB_STRINGS_XML)
 
-    message(STATUS "# READ ${FB_STRINGS_XML}")
-
     set(FB_STRINGS_IDX 0)
     foreach(LINE IN LISTS FB_STRINGS_XML)
         string(REGEX MATCH "<string name=\"([^\"]+)\"" _ "${LINE}")
         if(NOT "${CMAKE_MATCH_1}" STREQUAL "")
-            message(STATUS "String: ${CMAKE_MATCH_1}")
-
             set(FB_STRINGS_CPP_ENUM_LINES "${FB_STRINGS_CPP_ENUM_LINES}\t\t${CMAKE_MATCH_1} = ${FB_STRINGS_IDX},\n")
             set(FB_STRINGS_CPP_SWITCH_LINES "${FB_STRINGS_CPP_SWITCH_LINES}\tcase ${CMAKE_MATCH_1}:\n\t\tstrName = \"${CMAKE_MATCH_1}\";\n\t\tbreak;\n")
 
